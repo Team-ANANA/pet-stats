f89ba02151c1a5097f3ddf85e35a0d0db39bba06
Jason-HaoXinChao <jason@ChaoFamily.localdomain>
2022-10-09 21:07:25 -0400

    Add mock testcase
    The mock test will log some random text so we know the test suite is
    running correctly.

--- src/frontend/web/src/App.test.js
+++ src/frontend/web/src/App.test.js
@@ -6,3 +6,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('mock test', () => {
+  console.log("we just ran a test!\n");
+  console.log("The react tester is working hard to test the app!\n");
+});
