10d54ecb552b75a2981a06a370c5cb36c2646691
Jason <hao.chao@mail.utoronto.ca>
2022-11-10 01:53:12 -0500

    Add sql building in pie graph
    only very basic error test case has been added
    I have not tested if the sql execute code works, copied it from the
    dbloader module.

--- src/api/environments/api.py
+++ src/api/environments/api.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, make_response, request
+import mysql.connector
+from datetime import datetime
 
 app = Flask(__name__)
+param_names=['type', 'age', 'breed', 'gender', 'size', 'status', 'country', 'province']
 
 @app.get('/')
 def default_route():
@@ -10,24 +13,76 @@ def default_route():
 def hello():
     return 'Hello, World!'
 
-# Route for querying entries for all parameters
-@app.get('V0/data/entry/')
-def get_entries():
+# # Route for querying entries for all parameters
+# @app.get('V0/data/entry/')
+# def get_entries():
 
-    entries = {}
-    # TODO: fill in the entries with sql query
-    return make_response(jsonify(entries), 200)
+#     entries = {}
+#     # TODO: fill in the entries with sql query
+#     return make_response(jsonify(entries), 200)
 
 
 # Route for querying for pie graph
-@app.post('/V0/graph/pie/<int:category>')
-def get_pie_graph(category):
-    if len(category) == 0:
-        return make_response("", 400)
-    
-    # list of parameters to query with
+@app.post('/V0/graph/pie')
+def get_pie_graph():
+    # list of parameters in query
     parameters = request.get_json()
+
+    # category is mandatory for pie graph request
+    if 'category' in parameters:
+        category = parameters['category']
+    else:
+        return make_response("", 400)
+    if category not in param_names:
+        return make_response("", 400)
+        
+    # parse the tables for where conditions
+    where = "Where "
+    temp_table = ""
     
-    data = {}
-    # TODO: fill in the data with sql query
+    # only add condition if parameter is given
+    for param_name, entries in parameters.items():
+        if entries != [] and param_name not in ['dateEnd', 'dateBegin']:
+            temp_table += create_temp_table(entries, param_name)
+            where += f"({param_name} in (SELECT * FROM @{param_name})) AND"
+
+    # add date range in query
+    if 'dateBegin' in parameters and 'dateEnd' in parameters:
+        begin = datetime.strptime(parameters['dateBegin'])
+        begin = datetime.timestamp(begin)
+        where += f"(published_at >= {begin}) AND"
+        end = datetime.strptime(parameters['dateEnd'])
+        end = datetime.timestamp(end)
+        where += f"(published_at <= {end}"
+    else:
+        return make_response("", 400)
+
+    # construct the final sql statement.
+    sql = temp_table \
+        + f"SELECT COUNT(id), {category}" \
+        +  "FROM animals"\
+        + f"GROUP BY {category}" \
+        + where + ";"
+
+    # execute sql query
+    cnx = mysql.connector.connect(user='root', password='my-secret-pw', port="3306",
+                                host='localhost',
+                                database='pet-stats')
+    mycursor = cnx.cursor()
+    mycursor.execute(sql)
+    data = mycursor.fetchall()
+    cnx.close()
+    
     return make_response(jsonify(data), 200)
+
+def create_temp_table(array, table_name):
+    values = ""
+    for entry in array:
+        values += f",({entry})"
+    values = values[1:]
+    sql = f"""
+    DECLARE @{table_name} varchar(100);
+    INSERT INTO @{table_name} values{values};
+    """ 
+    return sql
+    
--- src/api/environments/tests/test_routes.py
+++ src/api/environments/tests/test_routes.py
@@ -1,5 +1,6 @@
 import pytest
-from api import app
+
+from api import app  # type: ignore
 
 @pytest.fixture
 def client():
@@ -16,6 +17,48 @@ def test_hello_world(client):
     assert response.status_code == 200
     assert b'Hello, World!' in response.data
 
-def test_get_pie_graph_no_category(client):
-    response = client.post('V0/data/entry/category="province"&type="dog"')
+def test_pie_graph_invalid_category(client):
+    response = client.post('/V0/graph/pie', json={
+        "type":"abc",
+        "age":"123",
+        "country":"canada",
+        "category":"1123332"
+    })
+    
+    assert response.status_code == 400
+
+def test_pie_graph_no_category(client):
+    response = client.post('/V0/graph/pie', json={
+        "type":"abc",
+        "age":"123",
+        "country":"canada"
+    })
+    
+    assert response.status_code == 400
+
+def test_pie_graph_no_json(client):
+    response = client.post('/V0/graph/pie')
+    
+    assert response.status_code == 400
+
+def test_pie_graph_no_date_begin(client):
+    response = client.post('/V0/graph/pie', json={
+        "type":"abc",
+        "age":"123",
+        "country":"canada",
+        "category":"type",
+        "dateEnd":"1111/11/11"
+    })
+    
+    assert response.status_code == 400
+
+def test_pie_graph_no_date_end(client):
+    response = client.post('/V0/graph/pie', json={
+        "type":"abc",
+        "age":"123",
+        "country":"canada",
+        "category":"type",
+        "dateBegin":"1111/11/11"
+    })
+    
     assert response.status_code == 400
