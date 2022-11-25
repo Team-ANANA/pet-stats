0e812763a00f87007f8ec807f943fc3c3050ac7f
angely67 <82392932+angely67@users.noreply.github.com>
2022-11-25 12:31:57 -0500

    add heatmap visualization, filters & testing. New dependency of d3 datamaps TS

--- src/frontend/web/src/App.test.js
+++ src/frontend/web/src/App.test.js
@@ -4,13 +4,13 @@ import {BrowserRouter, MemoryRouter} from 'react-router-dom'
 import App from './App';
 
 test('renders title of our website', () => {
-  render(<App />);
+  render(<App test={true}/>);
   const title = screen.getByText(/Pet Adoption Data Visualizer/i);
   expect(title).toBeInTheDocument();
 });
 
 test('renders navbar to 3 visualizations', () => {
-  render(<App />);
+  render(<App test={true}/>);
   const heatmap = screen.getByText("Map");
   expect(heatmap).toBeInTheDocument();
   const linegraph = screen.getByText("Line Graph");
@@ -19,6 +19,29 @@ test('renders navbar to 3 visualizations', () => {
   expect(piechart).toBeInTheDocument();
 });
 
+describe('Heatmap page is routed and displays', () => {
+  beforeEach(() => {
+      const renderWithRouter = (ui, { route = '/heatmap' } = {}) => {
+          window.history.pushState({}, 'Test page', route);
+
+          return render(ui);
+      };
+      renderWithRouter(<App test={true}/>);
+  });
+  test('should render without crashing', () => {});
+  test('should render heat map page', () => {
+      const heatmapPage = screen.getByText('Heat Map Visualizer');
+      expect(heatmapPage).toBeInTheDocument();
+  });
+  test('should render filters', () => {
+    const Filter = screen.getByText(/Breed/i);
+    expect(Filter).toBeInTheDocument();
+    const advanceSearch = screen.getByText(/Advance Search/i);
+    expect(advanceSearch).toBeInTheDocument();
+});
+});
+
+
 describe('Piechart page is routed and displays', () => {
   beforeEach(() => {
       const renderWithRouter = (ui, { route = '/piechart' } = {}) => {
@@ -26,7 +49,7 @@ describe('Piechart page is routed and displays', () => {
 
           return render(ui);
       };
-      renderWithRouter(<App />);
+      renderWithRouter(<App test={true}/>);
   });
   test('should render without crashing', () => {});
   test('should render pie chart page', () => {
