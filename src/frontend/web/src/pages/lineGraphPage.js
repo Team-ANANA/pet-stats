9cd7c7789814c5af8dcd3e3777c929ae24809558
osmanafandi <osman.afandiyev@mail.utoronto.ca>
2022-12-03 18:51:58 -0500

    passing some elements to the component through the graph view

--- src/frontend/web/src/pages/lineGraphPage.js
+++ src/frontend/web/src/pages/lineGraphPage.js
@@ -1,7 +1,18 @@
+import React, {useEffect, useState} from "react";
+import LineGraph from "../components/linegraph"
+import API_URL from "../env";
+
 function LineGraphPage() {
+
+    
     return (
       <>
-      LineGraphPage
+      <LineGraph data={ [
+            {"date": "2009-04-28", "value": "135.98", "sort": "cat"},
+            {"date": "2011-05-28", "value": "15.9338", "sort": "dog"},
+            {"date": "2013-09-22", "value": "38", "sort": "dog"},
+            {"date": "2014-06-28", "value": "90", "sort": "cat"}
+        ]}/>
     </>
     )
   }
