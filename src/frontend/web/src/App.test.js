559810da75741637fbd82a9bb0b95be7bf4b5586
Camran Hansen <camran.hansen@mail.utoronto.ca>
2022-12-07 22:58:04 -0500

    advance search -> advanced search

--- src/frontend/web/src/App.test.js
+++ src/frontend/web/src/App.test.js
@@ -36,7 +36,7 @@ describe('Heatmap page is routed and displays', () => {
   test('should render filters', () => {
     const Filter = screen.getByText(/Breed/i);
     expect(Filter).toBeInTheDocument();
-    const advanceSearch = screen.getByText(/Advance Search/i);
+    const advanceSearch = screen.getByText(/Advanced Search/i);
     expect(advanceSearch).toBeInTheDocument();
 });
 });
@@ -59,7 +59,7 @@ describe('Piechart page is routed and displays', () => {
   test('should render filters', () => {
     const Filter = screen.getByText(/Gender/i);
     expect(Filter).toBeInTheDocument();
-    const advanceSearch = screen.getByText(/Advance Search/i);
+    const advanceSearch = screen.getByText(/Advanced Search/i);
     expect(advanceSearch).toBeInTheDocument();
 });
 });
