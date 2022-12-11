02e92cac29e963d66428c6c2db9af7a0d54f7cf5
Camran Hansen <camran.hansen@mail.utoronto.ca>
2022-12-11 12:34:37 -0500

    add relevant GH action to upload to dockerhub

--- src/DB-api/environments/utils/coordinator.py
+++ src/DB-api/environments/utils/coordinator.py
@@ -42,7 +42,7 @@ def setup_db():
 
     # Load actual petfinder animals
     logging.info("Loading animals")
-    db_util.load_large_animal_dataset(7)
+    db_util.load_large_animal_dataset(1)
 
     logging.info("Loading successful, exiting.")
 
